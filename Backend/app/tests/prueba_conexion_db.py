71947414e959459d3b4d491941ff5dc7ee7f8006
Samuel Herrera <saherrerav@unal.edu.co>
2026-07-11 22:10:13 -0500

    feat: creacion modelos tablas para login

--- Backend/app/tests/prueba_conexion_db.py
+++ Backend/app/tests/prueba_conexion_db.py
@@ -1,6 +1,7 @@
 from app.core.database import SessionLocal
 from sqlalchemy import select, literal 
 
+#Prueba de conexion base de datos
 with SessionLocal() as session: 
     query = select(literal(50))
     
---
+++ Backend/app/tests/prueba_usuario.py
@@ -0,0 +1,12 @@
+from app.core.database import SessionLocal
+from app.models import Usuario
+from sqlalchemy import select
+
+with SessionLocal() as session:
+    query = select(Usuario).where(Usuario.rol == "Administrador")
+    
+    resultados = session.execute(query).scalars().all()
+    
+    print(f"Se encontraron {len(resultados)} resultados")
+    for resultado in resultados: 
+        print(resultado)
