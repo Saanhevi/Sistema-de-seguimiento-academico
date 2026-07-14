f01eb435db2f5c7fe574e83e531425cf3ff3ddb7
Samuel Herrera <saherrerav@unal.edu.co>
2026-07-14 08:57:41 -0500

    fix : correcion servicio auth

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -1,25 +1,14 @@
 # Direccionamiento de las rutas de autenticación
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
 from app.schemas.auth import LoginRequest, TokenResponse
-from app.services import auth as auth_service
-from app.core.database import SessionLocal # Fábrica de sesiones
-
+from app.services.auth import AuthService
+from app.core.dependencies import get_auth_service
 router = APIRouter(
     prefix="/api/auth",
     tags=["Autenticación"]
 )
 
-# Dependencia de FastAPI: Abre y cierra la conexión con PostgreSQL
-def get_db():
-    db = SessionLocal() # Abre una conexión limpia usando el motor
-    try:
-        yield db        # Le entrega la conexión a la ruta que la solicitó
-    finally:
-        db.close()      # Cuando la ruta termina su trabajo, cierra la conexión pase lo que pase
-
-
 @router.post("/login", response_model=TokenResponse)
-def login(credentials: LoginRequest, db: Session = Depends(get_db)):
+def login(credentials: LoginRequest, service : AuthService = Depends(get_auth_service) ):
     #pasamos las credenciales a servicio para que se verifiquen allí
-    return auth_service.autenticar_usuario(credentials, db)
+    return service.autenticar_usuario(credentials)
--- Backend/app/services/auth.py
+++ Backend/app/services/auth.py
@@ -4,41 +4,50 @@ from sqlalchemy.orm import Session
 from app.schemas.auth import LoginRequest
 from app.models.usuario import Usuario
 from app.core.security import controlador_contrasena, create_access_token
+from app.repositories.usuario import UsuarioRepository
 
-def autenticar_usuario(credentials: LoginRequest, db: Session) -> dict:
-    """
-    Busca al usuario en la base de datos, valida su contraseña y genera el JWT
-    """
-    # 1. Buscamos al usuario por correo
-    user = db.query(Usuario).filter(Usuario.correo==credentials.correo).first()
-    if not user:
-        raise HTTPException()(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Correo o contraseña incorrectos!"
-        )
-    #2 Verificamos la contraseña con el controlador
-    clave_correcta = controlador_contrasena.verificar_contrasena(
-        credentials.password,
-        user.password_hash
-    )
-
-    if not clave_correcta:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Correo o contraseña incorrectos!!"
-        )
+class AuthService: 
     
-    # 3. Generamos el token
-    token_data = {"sub": str(user.id_usuario), "rol": user.rol}
-    token_real = create_access_token(data=token_data)
+    def __init__(self, session: Session, repositorio:UsuarioRepository):
+        self.session = session
+        self.repositorio = repositorio
+        
+    def autenticar_usuario(self,credentials: LoginRequest) -> dict:
+        """
+        Busca al usuario en la base de datos, valida su contraseña y genera el JWT
+        """
+        with self.session as session:
+            # 1. Buscamos al usuario por correo
+            user = self.repositorio.buscar_por_correo(credentials.correo)
+            #user = db.query(Usuario).filter(Usuario.correo==credentials.correo).first()
+            if not user:
+                raise HTTPException()(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Correo o contraseña incorrectos!"
+                )
+            #2 Verificamos la contraseña con el controlador
+            clave_correcta = controlador_contrasena.verificar_contrasena(
+                credentials.password,
+                user.password_hash
+            )
 
-    # 4. Devolver el formato plano
-    return{
-        "access_token": token_real,
-        "token_type": "bearer",
-        "rol": user.rol,
-        "nombres": user.nombres
-    }
+            if not clave_correcta:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Correo o contraseña incorrectos!!"
+                )
+            
+            # 3. Generamos el token
+            token_data = {"sub": str(user.id_usuario), "rol": user.rol}
+            token_real = create_access_token(data=token_data)
+
+            # 4. Devolver el formato plano
+            return{
+                "access_token": token_real,
+                "token_type": "bearer",
+                "rol": user.rol,
+                "nombres": user.nombres
+            }
 
 # def credentials_verification(credentials: LoginRequest):
 #     # Simulación de base de datos
