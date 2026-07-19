35e488ffe5129cefd9a73527e66b55256be61fd4
snehider <jsalasr@unal.edu.co>
2026-07-18 19:40:22 -0500

    refactor: organizar estilos y cambio en el docker compuse
    dividi el archivo original de los estilos en varios estilos tipo modelares para mejorar el orden.
    cambie el estilo del login.
    y en el docker compuse cambie esto: command: ["python", "crear_usuario_test.py"]  por  command: ["python", "-m", "app.tests.crear_usuario_test"]
    ya que no me funcionaba (el cambio fue sugerido por copilot).

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,11 @@
-import "./styles.css";
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/Topbar.css";
+import "./styles/Navbar.css";
+import "./styles/Dashboard.css";
+import "./styles/Docente.css";
+import "./styles/Estudiante.css";
+import "./styles/Login.css";
 import AppRouter from "./routes/AppRouter";
 
 export default function App() {
