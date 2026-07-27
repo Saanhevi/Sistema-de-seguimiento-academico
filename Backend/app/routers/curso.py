857c8199435689d9a885f2d4ca5f4a287bdf0bdc
Santiago <santiagocardenasr123@gmail.com>
2026-07-27 15:27:03 -0500

    fix: HU2 exclusivamente para administrador

--- Backend/app/routers/curso.py
+++ Backend/app/routers/curso.py
@@ -110,7 +110,7 @@ def obtener_curso(
 def crear_matricula(
     payload: MatriculaCreate,
     service: CursoService = Depends(get_curso_service),
-    usuario=Depends(require_role("Administrador", "Docente")),
+    usuario=Depends(require_role("Administrador")),
 ):
     return service.crear_matricula(payload.id_estudiante, payload.id_grado, payload.anio, usuario_actual=usuario)
 
